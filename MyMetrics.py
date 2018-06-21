9c0664d931a257bc34767071051641b41a2d2a78
dmandache <diana.mandache00@gmail.com>
2018-06-21 14:23:01 +0200

    add specificity and sensitivity metrics

--- MyMetrics.py
+++ MyMetrics.py
@@ -53,3 +53,19 @@ def recall(y_true, y_pred):
     recall = c1 / c3
 
     return recall
+
+def sensitivity(y_true, y_pred):
+
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    sensitivity = true_positives / (possible_positives + K.epsilon())
+
+    return sensitivity
+
+def specificity(y_true, y_pred):
+
+    true_negatives = K.sum(K.round(K.clip((1-y_true) * (1-y_pred), 0, 1)))
+    possible_negatives = K.sum(K.round(K.clip(1-y_true, 0, 1)))
+    specificity = true_negatives / (possible_negatives + K.epsilon())
+
+    return specificity
