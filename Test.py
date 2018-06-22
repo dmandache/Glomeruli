6e2fa9c04f36db87b375a0fff5fd273f307ea4ab
dmandache <diana.mandache00@gmail.com>
2018-06-22 16:05:31 +0200

    pil resize issues

--- Test.py
+++ Test.py
@@ -7,26 +7,77 @@ import argparse
 
 import numpy as np
 from PIL import Image
+from scipy.misc import imsave
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 def get_n_samples(n=32, dir=None, target_size=(299,299)):
     samples = []
     files = os.listdir(dir)
-    for _ in range(n):
+    random.seed(9001)
+
+    for i in range(n):
         random_file = random.choice(files)
         path_to_random_file = dir + '/' + random_file
-        x = Image.open(path_to_random_file)
+        try:
+            img = Image.open(path_to_random_file)
+        except IOError:
+            i = i-1
+            continue
         if target_size is not 'original':
-            x = Image.Image.resize(x,target_size)
-        x = np.asarray(x, dtype='float32')
+            img_new = Image.Image.resize(img,target_size, resample=Image.BICUBIC)
+        else: img_new = img
+        x = np.asarray(img_new, dtype='float32')
         x /= 255
         samples.append(x)
+
+    samples = np.stack(samples, axis=0)
+    #samples = np.expand_dims(samples, axis=3)
+
     return samples
 
-def preprocess(img):
-    x = np.asarray(img, dtype='float32')
-    x /= 255
-    #x = np.expand_dims(x, axis=3)
-    return x
+def plot_to_grid(batch, name='images', grid_size=7, random=False):
+    # img_width, img_height = patches[0].shape
+    img_size = batch[0].shape[0]
+    try:
+        ch =  batch[0].shape[2]
+    except:
+        ch = 1
+    nt = len(batch)
+    n = grid_size * grid_size  # display n random patches
+
+    if n <= nt:
+        if random:
+            kept_idxs = random.sample(range(nt), n)
+        else:
+            kept_idxs = range(n)
+        kept_patches = [batch[i] for i in kept_idxs]
+    else:
+        n0 = n - nt
+        kept_patches = list(batch)
+        for i in range(n0):
+            kept_patches.append(np.zeros((img_size, img_size, ch)))
+
+    # build a black picture with enough space for
+    # our 8 x 8 filters of size 128 x 128, with a 5px margin in between
+    margin = 3
+    width = grid_size * img_size + (grid_size - 1) * margin
+    height = grid_size * img_size + (grid_size - 1) * margin
+    stitched_images = np.zeros((width, height, ch))
+
+    # fill the picture with our saved filters
+    for i in range(grid_size):
+        for j in range(grid_size):
+            img = kept_patches[i * grid_size + j]
+            stitched_images[(img_size + margin) * i: (img_size + margin) * i + img_size,
+            (img_size + margin) * j: (img_size + margin) * j + img_size, ] = img
+
+    # save the result to disk
+    # imsave('./patches_%s_%d-%d.png' % (name, n, nt), stitched_patches)
+    imsave('./output/%s.png' % name, stitched_images)
+
 
 def main(dir=None):
     if dir == None:
@@ -39,23 +90,25 @@ def main(dir=None):
     DIR_TRAIN_NONGLOM = IMAGES_DIR_PATH + "/train/00_nonglomeruli"
     DIR_TEST_NONGLOM = IMAGES_DIR_PATH + "/test/00_nonglomeruli"
 
-    nb_samples = 32
-
     # Load some images and preprocess them
-    x_test_glom = get_n_samples(nb_samples, dir=DIR_TEST_GLOM, target_size=(299,299))
-    x_test_nonglom = get_n_samples(nb_samples, dir=DIR_TEST_NONGLOM, target_size=(299,299))
+    x_test_glom = get_n_samples(NB_SAMPLES, dir=DIR_TEST_GLOM, target_size=(299,299))
+    x_test_nonglom = get_n_samples(NB_SAMPLES, dir=DIR_TEST_NONGLOM, target_size=(299,299))
 
-    model = load_model('./output/model.hdf5', custom_objects={'sensitivity': sensitivity, 'specificity': specificity} )
+    model = load_model('./output/model.hdf5', custom_objects={'sensitivity': sensitivity, 'specificity': specificity})
+
+    plot_to_grid(x_test_glom, name='glomeruli_examples', grid_size=7)
+
+    plot_to_grid(x_test_nonglom, name='non-glomeruli_examples', grid_size=7)
 
     y_test_glom = model.predict(x_test_glom)
     print(y_test_glom)
+    #y_test_nonglom = model.predict(x_test_nonglom)
 
-    y_test_nonglom = model.predict(x_test_nonglom)
-    print(y_test_nonglom)
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
+    NB_SAMPLES = 50
     parser.add_argument('--dir', help='data directory')
     args = parser.parse_args()
 
